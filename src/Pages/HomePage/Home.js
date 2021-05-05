e11b2cacadff0a9b87d903fb7d6ba0b94bb65ac0
adarshajit <adarshajit@gmail.com>
2021-05-05 18:42:14 +0530

    react reveal animation added 🌠

--- src/Pages/HomePage/Home.js
+++ src/Pages/HomePage/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Bounce from "react-reveal/Bounce";
 import {
   HomeContainer,
   HeroImg,
@@ -14,25 +15,28 @@ import Tech from "../HomePage/HomeSections/TechSection/Tech";
 import Education from "../HomePage/HomeSections/EducationSection/Education";
 import Organisations from "../HomePage/HomeSections/OrgSection/Org";
 import Contact from "./HomeSections/ContactSection/Contact";
+
 const Home = () => {
   return (
     <>
       <HomeContainer>
-        <AvatarContainer>
-          <HeroImg src={Avatar} />
-        </AvatarContainer>
+        <Bounce top>
+          <AvatarContainer>
+            <HeroImg src={Avatar} />
+          </AvatarContainer>
 
-        <TextContainer>
-          <HeroText>Hey There! 👋</HeroText>
-          <TextInfo>
-            Welcome to my space in the web. Thanks for coming by. Here you can
-            learn about my journey so far.
-          </TextInfo>
-          <ButtonContainer>
-            <Button>Let's Talk</Button>
-            <Button>Resume</Button>
-          </ButtonContainer>
-        </TextContainer>
+          <TextContainer>
+            <HeroText>Hey There! 👋</HeroText>
+            <TextInfo>
+              Welcome to my space in the web. Thanks for coming by. Here you can
+              learn about my journey so far.
+            </TextInfo>
+            <ButtonContainer>
+              <Button>Let's Talk</Button>
+              <Button>Resume</Button>
+            </ButtonContainer>
+          </TextContainer>
+        </Bounce>
       </HomeContainer>
       <Tech />
       <Education />
